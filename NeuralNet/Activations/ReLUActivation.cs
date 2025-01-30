f9ff2a37bf779b84469701eb2c47828a62222641
Stanislav Stoychev <stanislav.v.stoychev@gmail.com>
2025-01-30 11:17:47 +0200

    Renames + ReLU activation

---
+++ NeuralNet/Activations/ReLUActivation.cs
@@ -0,0 +1,10 @@
+﻿using NN;
+
+namespace NeuralNet.Activations;
+
+public class ReLUActivation : IActivation
+{
+    public double Activation(double input) => input >= 0 ? input : 0;
+
+    public double ActivationDerivative(double input) => input >= 0 ? 1 : 0;
+}
